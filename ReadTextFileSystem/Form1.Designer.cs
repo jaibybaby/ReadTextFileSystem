549257cd165a95cd3eca1fe952293c522c86ae1e
jaibymol4@gmail.com <jaibymol4@gmail.com>
2021-05-16 23:03:43 +0530

    Add project files.

---
+++ ReadTextFileSystem/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿
+namespace ReadTextFileSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPlace = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblPageNumber = new System.Windows.Forms.Label();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtPhoneNumber = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(11, 52);
+            this.txtName.Margin = new System.Windows.Forms.Padding(2);
+            this.txtName.Multiline = true;
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(485, 41);
+            this.txtName.TabIndex = 2;
+            // 
+            // txtPlace
+            // 
+            this.txtPlace.Location = new System.Drawing.Point(11, 132);
+            this.txtPlace.Margin = new System.Windows.Forms.Padding(2);
+            this.txtPlace.Multiline = true;
+            this.txtPlace.Name = "txtPlace";
+            this.txtPlace.Size = new System.Drawing.Size(485, 41);
+            this.txtPlace.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblPageNumber);
+            this.panel1.Controls.Add(this.btnPrevious);
+            this.panel1.Controls.Add(this.btnNext);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtEmail);
+            this.panel1.Controls.Add(this.txtPhoneNumber);
+            this.panel1.Controls.Add(this.txtPlace);
+            this.panel1.Controls.Add(this.txtName);
+            this.panel1.Location = new System.Drawing.Point(20, 4);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(508, 408);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // lblPageNumber
+            // 
+            this.lblPageNumber.AutoSize = true;
+            this.lblPageNumber.Location = new System.Drawing.Point(11, 3);
+            this.lblPageNumber.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblPageNumber.Name = "lblPageNumber";
+            this.lblPageNumber.Size = new System.Drawing.Size(0, 13);
+            this.lblPageNumber.TabIndex = 11;
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnPrevious.Location = new System.Drawing.Point(14, 352);
+            this.btnPrevious.Margin = new System.Windows.Forms.Padding(2);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(85, 31);
+            this.btnPrevious.TabIndex = 10;
+            this.btnPrevious.Text = "Previous";
+            this.btnPrevious.UseVisualStyleBackColor = false;
+            this.btnPrevious.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnNext.Location = new System.Drawing.Point(414, 352);
+            this.btnNext.Margin = new System.Windows.Forms.Padding(2);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(85, 31);
+            this.btnNext.TabIndex = 9;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = false;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 266);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "E-mail Id";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 186);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Phone Number";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 106);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Place";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 26);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Name";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(11, 292);
+            this.txtEmail.Margin = new System.Windows.Forms.Padding(2);
+            this.txtEmail.Multiline = true;
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(485, 41);
+            this.txtEmail.TabIndex = 4;
+            // 
+            // txtPhoneNumber
+            // 
+            this.txtPhoneNumber.Location = new System.Drawing.Point(11, 212);
+            this.txtPhoneNumber.Margin = new System.Windows.Forms.Padding(2);
+            this.txtPhoneNumber.Multiline = true;
+            this.txtPhoneNumber.Name = "txtPhoneNumber";
+            this.txtPhoneNumber.Size = new System.Drawing.Size(485, 41);
+            this.txtPhoneNumber.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(554, 429);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "StudentDetails";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtPlace;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtPhoneNumber;
+        private System.Windows.Forms.Label lblPageNumber;
+    }
+}
+
---
+++ ReadTextFileSystem/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Activities.Expressions;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReadTextFileSystem
+{
+    public partial class Form1 : Form
+    {
+        string data;
+        string[] page = new string[5];
+        string[] names = new string[5];
+        string[] places = new string[5];
+        string[] phone = new string[5];
+        string[] email = new string[5];
+        int buttonClickCount = 1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            data = File.ReadAllText("D:\\one_team\\FileOperations\\ReadTextFileSystem\\MyData.txt");
+            data.TrimEnd(' ');
+            data = Regex.Replace(data, "\\s", "");
+            page = data.Split(' ', ')', '/');
+            names = data.Split(')', '+');
+            places = data.Split('+', '|');
+            phone = data.Split('|', '*');
+            email = data.Split('*', '/');
+            if (page[0] == "1")
+            {
+                txtName.Text = names[2];
+                txtPlace.Text = places[1];
+                txtPhoneNumber.Text = phone[1];
+                txtEmail.Text = email[1];
+                lblPageNumber.Text = "1/6";
+            }
+        }
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            buttonClickCount = buttonClickCount + 1;
+            if (page[3] == buttonClickCount.ToString())
+            {
+                txtName.Text = names[5];
+                txtPlace.Text = places[3];
+                txtPhoneNumber.Text = phone[3];
+                txtEmail.Text = email[3];
+                lblPageNumber.Text = "2/6";
+            }
+            if (page[6] == buttonClickCount.ToString())
+            {
+                txtName.Text = names[8];
+                txtPlace.Text = places[5];
+                txtPhoneNumber.Text = phone[5];
+                txtEmail.Text = email[5];
+                lblPageNumber.Text = "3/6";
+            }
+            if (page[9] == buttonClickCount.ToString())
+            {
+                txtName.Text = names[11];
+                txtPlace.Text = places[7];
+                txtPhoneNumber.Text = phone[7];
+                txtEmail.Text = email[7];
+                lblPageNumber.Text = "4/6";
+            }
+            if (page[12] == buttonClickCount.ToString())
+            {
+                txtName.Text = names[14];
+                txtPlace.Text = places[9];
+                txtPhoneNumber.Text = phone[9];
+                txtEmail.Text = email[11];
+                lblPageNumber.Text = "5/6";
+            }
+            if (page[15] == buttonClickCount.ToString())
+            {
+                txtName.Text = names[17];
+                txtPlace.Text = places[11];
+                txtPhoneNumber.Text = phone[11];
+                txtEmail.Text = email[11];
+                lblPageNumber.Text = "6/6";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            buttonClickCount = buttonClickCount - 1;
+            if (page[0] == buttonClickCount.ToString())
+            {
+                txtName.Text = names[2];
+                txtPlace.Text = places[1];
+                txtPhoneNumber.Text = phone[1];
+                txtEmail.Text = email[1];
+                lblPageNumber.Text = "1/6";
+            }
+            if (page[3] == buttonClickCount.ToString())
+            {
+                txtName.Text = names[5];
+                txtPlace.Text = places[3];
+                txtPhoneNumber.Text = phone[3];
+                txtEmail.Text = email[3];
+                lblPageNumber.Text = "2/6";
+            }
+            if (page[6] == buttonClickCount.ToString())
+            {
+                txtName.Text = names[8];
+                txtPlace.Text = places[5];
+                txtPhoneNumber.Text = phone[5];
+                txtEmail.Text = email[5];
+                lblPageNumber.Text = "3/6";
+            }
+            if (page[9] == buttonClickCount.ToString())
+            {
+                txtName.Text = names[11];
+                txtPlace.Text = places[7];
+                txtPhoneNumber.Text = phone[7];
+                txtEmail.Text = email[7];
+                lblPageNumber.Text = "4/6";
+            }
+            if (page[12] == buttonClickCount.ToString())
+            {
+                txtName.Text = names[14];
+                txtPlace.Text = places[9];
+                txtPhoneNumber.Text = phone[9];
+                txtEmail.Text = email[11];
+                lblPageNumber.Text = "5/6";
+            }
+            if (page[15] == buttonClickCount.ToString())
+            {
+                txtName.Text = names[17];
+                txtPlace.Text = places[11];
+                txtPhoneNumber.Text = phone[11];
+                txtEmail.Text = email[11];
+                lblPageNumber.Text = "6/6";
+            }
+        }
+    }
+}
+
